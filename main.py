a24fa8d4b1b51c48487fb7feb9810ef3f1a40306
Yuseonr <rafianandra@gmail.com>
2025-07-16 12:41:30 +0700

    feat : Screen window using pygame

---
+++ main.py
@@ -0,0 +1,28 @@
+# 16 July 2025
+
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    while True : 
+        # Close Program
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        # Screen Fill
+        screen.fill('Black')
+        pygame.display.flip()
+
+
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
